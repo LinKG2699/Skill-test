b234c286484032b7b9f5d0cafee8c41d3246a78f
Gavin Lin <47804418+LinKG2699@users.noreply.github.com>
2019-03-13 12:57:13 +0800

    Add files via upload

---
+++ skill test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	double;
+	int H, I;
+	cout << "Summer job pays $15.50 and hour" << endl;
+	cout << "Number of hours worked" << endl;
+	cin >> H;
+	cout << "Income from summer fob before taxes" << endl;
+	cout << H * 15.50 << endl;
+	cout << "Income from summer job after taxes" << endl;
+	cout << (H * 15.50)*0.86 << endl;
+	cout << "Money spent on clothes and other accessories" << endl;
+	cout << (H *1.333) << endl;
+	cout << "Money spent on school supplies" << endl;
+	cout << H * .1333 << endl;
+	cout << "Money spent on saving bonds" << endl;
+	cout << (H * 11.8637)* 0.25 << endl;
+	cout << "Money parents spent to buy additional saving bonds" << endl;
+	cout <<(H* 2.965925)*0.5 << endl;
+	_getch();
+	return 0;
+}
